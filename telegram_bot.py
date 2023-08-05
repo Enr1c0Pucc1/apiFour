75dd8f21b928f5c4386ceadb30fb394e86f14a54
Pucci <greenymanxcreative@gmail.com>
2023-08-05 13:26:32 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,4 @@
 import telegram
 bot = telegram.Bot(token='6044295717:AAHzgphndfHFiMo_qUz02PJhVISoKlHCu_M')
 chat_id = bot.get_updates()[-1].message.chat_id
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id=chat_id, document=open('images/nasa_apod_1.jpg', 'rb'))
